045f65b96a69b756549491b9ab0ca990ebb1adf9
Saransh Rai <saranshrai07@gmail.com>
2026-01-18 00:05:17 +0530

    Update ansibleK8sDeploy.groovy

--- vars/ansibleK8sDeploy.groovy
+++ vars/ansibleK8sDeploy.groovy
@@ -1,1 +1,30 @@
-hi
+def call(String configFilePath = 'resources/ansible_config.yaml') {
+    // Load configuration
+    def config = readYaml file: "${configFilePath}"
+
+    stage('Clone Repo') {
+        echo "Cloning Ansible role from GitHub..."
+        git branch: 'saransh', url: 'https://github.com/OT-MyGurukulam/Ansible_33.git'
+    }
+
+    if (config.KEEP_APPROVAL_STAGE) {
+        stage('User Approval') {
+            input message: "Deploy Kubernetes to ${config.ENVIRONMENT}?", ok: 'Deploy'
+        }
+    }
+
+    stage('Execute Ansible Playbook') {
+        echo "Running Ansible playbook..."
+        sh """
+            ansible-playbook -i inventory Assignment-5/kuberole/playbook.yml
+        """
+    }
+
+    stage('Notification') {
+        echo "Sending notification to Slack channel ${config.SLACK_CHANNEL_NAME}"
+        slackSend channel: config.SLACK_CHANNEL_NAME, 
+                  color: 'good', 
+                  message: config.ACTION_MESSAGE
+    }
+}
+
